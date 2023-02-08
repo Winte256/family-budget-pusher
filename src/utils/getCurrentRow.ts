517acc3d0972ea67ca54fa49595f2cb0ab6c5138
m.krutihin <m.krutihin@gmail.com>
2023-02-08 13:28:29 +0300

    fix get day of month

--- src/utils/getCurrentRow.ts
+++ src/utils/getCurrentRow.ts
@@ -1,9 +1,11 @@
 
 export default () => {
   // set MSK timezone
-  const date =  new Date((new Date).toLocaleString('ru-RU', {
+  const date =  (new Date).toLocaleString('ru-RU', {
     timeZone: 'Europe/Moscow'
-  }));;
+  });;
+
+  const dayOfMonth = Number(date.split('.')[0])
   
-  return date.getDate();
+  return dayOfMonth;
 }
